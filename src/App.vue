d0f57ea77afe6287d5bb85462d24fdfb4fb6ce7c
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2021-07-15 14:53:22 +0100

    Finished reaction time

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Amy's reaction Timer</h1>
+ <button @click="begin" :disabled="isPlaying">Start Game</button>
+ <Block v-if="isPlaying" :delay="delay" @end="endGame" /> 
+ <Results v-if="showResults" :score="score" @end="endGame" />
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Block from './components/Block.vue' 
+  import Results from './components/Results.vue' 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Block, Results },
+  data (){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    begin(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+    endGame(reactionTime){
+      this.score = reactionTime,
+      this.isPlaying = false
+      this.showResults = true
+    }
 }
+}
+
 </script>
 
 <style>
@@ -20,7 +42,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="results">
+      <h2>Your score is: {{score}} ms</h2>
+      <p class="rank">{{rank}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score',],
+    data(){
+     return{
+         rank: null
+     } 
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Super fast reflexes!'
+        } else if(this.score < 500){
+            this.rank = 'Fast, but could be faster'
+        } else {
+            this.rank = 'Too Slow, go again'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
